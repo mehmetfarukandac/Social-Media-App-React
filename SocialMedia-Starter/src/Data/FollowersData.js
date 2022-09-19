8cf50c0fa14f8e8fd98292d8ae7e4eb0e9ffadf3
Mehmet Faruk Andaç <83770786+mehmetfarukandac1@users.noreply.github.com>
2022-09-19 23:13:26 +0300

    Followers Card
    I added followers Card Component to Profile Side and that was the last component for this side. So next step is the posts

---
+++ SocialMedia-Starter/src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/img1.jpg";
+import img2 from "../img/img2.jpg";
+import img3 from "../img/img3.jpg";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  { name: "Jorge Jesus", username: "@jeje", img: img4 },
+  { name: "Fatih Terim", username: "@fterim", img: img2 },
+  { name: "Neymar Jr", username: "@jrneymar", img: img3 },
+  { name: "Ronaldo Nazário", username: "@rronaldo", img: img1 },
+];
--- SocialMedia-Starter/src/components/profileSide/ProfileSide.jsx
+++ SocialMedia-Starter/src/components/profileSide/ProfileSide.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FollowersCard from "../followersCard/FollowersCard";
 import LogoSearch from "../logoSearch/LogoSearch";
 import ProfileCard from "../profileCard/ProfileCard";
 
@@ -8,6 +9,7 @@ const ProfileSide = () => {
     <div className="ProfileSide">
       <LogoSearch />
       <ProfileCard />
+      <FollowersCard />
     </div>
   );
 };

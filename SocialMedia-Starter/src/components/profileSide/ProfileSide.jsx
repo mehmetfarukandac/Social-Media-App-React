f04da85f30edd19037bbe6bda2dd0e35f2cbf0f2
Mehmet Faruk Andaç <83770786+mehmetfarukandac1@users.noreply.github.com>
2022-09-19 20:15:31 +0300

    LogoSearch for ProfileSide
    I added the search bar in the Profile side.

---
+++ SocialMedia-Starter/src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LogoSearch from "../logoSearch/LogoSearch";
+const ProfileSide = () => {
+  return (
+    <div className="profileSide">
+      <LogoSearch />
+    </div>
+  );
+};
+export default ProfileSide;

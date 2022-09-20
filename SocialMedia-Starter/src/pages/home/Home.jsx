4aea8ecad3fe105e732fb63b7dec70f3ad7449c3
Mehmet Faruk Andaç <83770786+mehmetfarukandac1@users.noreply.github.com>
2022-09-20 13:15:36 +0300

    Right Side
    The Right Side component has 2 components in it and the first one is about icons. Now , I added the icons and gave them some css. They seems like really cool

--- SocialMedia-Starter/src/pages/home/Home.jsx
+++ SocialMedia-Starter/src/pages/home/Home.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import PostSide from "../../components/postSide/PostSide";
 import ProfileSide from "../../components/profileSide/ProfileSide";
-import "./Home.css"
+import RightSide from "../../components/rightSide/RightSide";
+import "./Home.css";
 
-const Home = ()=> {
-  return <div className="Home">
-    <ProfileSide />
-    <PostSide />
-    <div className="rightSide">Right Side</div>
-  </div>;
-}
+const Home = () => {
+  return (
+    <div className="Home">
+      <ProfileSide />
+      <PostSide />
+      <RightSide />
+    </div>
+  );
+};
 
 export default Home;
-

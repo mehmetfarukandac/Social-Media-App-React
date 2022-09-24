77a1ceeee7bad7af9daf25a6899874ed30760625
Mehmet Faruk Andaç <83770786+mehmetfarukandac1@users.noreply.github.com>
2022-09-24 13:10:06 +0300

    Models,Controller and Routers
    I added Models,Controller and Routers for auth and user

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import AuthRoute from "./Routes/AuthRoute.js";
+import UserRoute from "./Routes/UserRoute.js";
+// ROUTES
 
 const app = express();
+
+// Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
@@ -22,3 +27,7 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+// usage of routes
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
